614aa5783347486fb8b08a0124a0bc1bf2659de8
mikando <iletisim@mehmetakifcakar.com>
2019-05-17 11:20:34 +0300

    Initial version

---
+++ procExecutor/ExecutorConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace procExecutor
+{
+    public class ExecutorConfig
+    {
+        public string ProcessId { get; set; }
+
+        public string AssemblyPath { get; set; }
+
+        public string ClassType { get; set; }
+
+        public string MethodName { get; set; }
+
+        public int? TimeOutInMilliSeconds { get; set; }
+    }
+}
---
+++ procExecutor/Program.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace procExecutor
+{
+    class Program
+    {
+        static ExecutorConfig config;
+        public static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1)
+                {
+                    throw new Exception("config file is missing");
+                }
+                if (System.IO.File.Exists(args[0]) == false)
+                {
+                    throw new Exception("config file not found");
+                }
+                var content = System.IO.File.ReadAllText(args[0], new UTF8Encoding(false));
+                config = JsonConvert.DeserializeObject<ExecutorConfig>(content);
+                using (var mutex = new Mutex(true, config.ProcessId, out bool createdNew))
+                {
+                    if (createdNew)
+                    {
+                        var cts = new CancellationTokenSource();
+                        if (config.TimeOutInMilliSeconds.HasValue)
+                        {
+                            cts.CancelAfter(config.TimeOutInMilliSeconds.Value);
+                        }
+                        Console.WriteLine("Executing");
+                        Task.Run(run, cts.Token).GetAwaiter().GetResult();
+                        Console.WriteLine("Executed");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Allready running");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+        }
+
+        private static async Task run()
+        {
+            Assembly testAssembly = Assembly.LoadFile(config.AssemblyPath);
+            Type classType = testAssembly.GetType(config.ClassType);
+            var method = classType.GetMethod(config.MethodName, BindingFlags.Public | BindingFlags.Static);
+            await (Task)method.Invoke(null, null);
+        }
+    }
+}
